9c634a363d3f520a2f26e9bb4c5ecad7ae430099
Nazmul-vs-code <khalekbiton1977@gmail.com>
2026-07-11 08:04:14 +0600

    admin functionality for restaurant pproval and deletion was added

---
+++ src/app/dashboard/admin/restaurants/page.tsx
@@ -0,0 +1,48 @@
+import AdminRestaurantCard from "@/components/dashboard/admin/AdminRestaurantCard";
+import { GetRestaurants } from "@/lib/api/restaurants";
+
+type Restaurant = {
+    _id: string;
+    restaurantName: string;
+    ownerName: string;
+    ownerEmail: string;
+    location: string;
+    image: string;
+    phone: string;
+    description: string;
+    status: "pending" | "approved" | "rejected";
+};
+
+const AdminRestaurantsPage = async () => {
+    const restaurants = await GetRestaurants("/api/restaurants");
+
+    return (
+        <section className="space-y-6">
+
+            <div>
+                <h1 className="text-3xl font-bold text-red-500">
+                    Manage Restaurants 🍽️
+                </h1>
+
+                <p className="mt-1 text-gray-500">
+                    Total Restaurants:{" "}
+                    <span className="font-semibold text-red-500">
+                        {restaurants.length}
+                    </span>
+                </p>
+            </div>
+
+            <div className="space-y-5">
+                {restaurants.map((restaurant: Restaurant) => (
+                    <AdminRestaurantCard
+                        key={restaurant._id}
+                        restaurant={restaurant}
+                    />
+                ))}
+            </div>
+
+        </section>
+    );
+};
+
+export default AdminRestaurantsPage;
